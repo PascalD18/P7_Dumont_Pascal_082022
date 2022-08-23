b298c5a16df0a9b3b11e6e3979654b33625f55cc
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-08-23 11:09:35 +0200

    maj

--- Backend/models/Sauce.js
+++ Backend/models/Sauce.js
@@ -1,17 +1,8 @@
 const mongoose = require('mongoose');
 
 const SauceSchema = mongoose.Schema({
-    userId: { type: String, require: true },
-    name: { type: String, require: true },
-    manufacturer: { type: String, require: true },
-    description: { type: String, require: true },
-    mainPepper: { type: String, require: true },
-    imageUrl: { type: String, require: false },
-    heat: { type: Number, require: true, default: 1 },
-    likes: { type: Number, require: true, default: 0 },
-    dislikes: { type: Number, require: true, default: 0 },
-    usersLiked: { type: [String], require: true, default: [] },
-    usersDisliked: { type: [String], require: true, default: [] },
+    userId: { type: String},
+    name: { type: String},
 }
 )
 module.exports = mongoose.model('Sauce', SauceSchema);
